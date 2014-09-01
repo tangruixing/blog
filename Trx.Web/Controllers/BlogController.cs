3e57a3e544338ce7d2ce21b82baf0a77cd3ddb32
https://github.com/tangruixing/blog.git <tangruixing@163.com>
2014-09-01 21:48:40 +0800

    aaa

--- Trx.Web/Controllers/BlogController.cs
+++ Trx.Web/Controllers/BlogController.cs
@@ -15,7 +15,7 @@ namespace Trx.Web.Controllers
         public ActionResult Index()
         {
             var db = new BlogDBContext();
-            db.Articles.Add(new Article { ArticleContent = "asdfsadfasd", ArticleId = Guid.NewGuid(), CreateTime = DateTime.Now,ModifyTime = DateTime.Now,IsDelete = false });
+            db.Articles.Add(new Article { ArticleContent = "aaa", ArticleId = Guid.NewGuid(), CreateTime = DateTime.Now,ModifyTime = DateTime.Now,IsDelete = false });
             db.SaveChanges();
 
             return View(db.Articles.ToList());
