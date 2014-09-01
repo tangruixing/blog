ec3bdd9f08a5f78a70bc16dbf73e4d45a319f4b5
tangruixing <tangruixing@163.com>
2014-09-01 21:25:01 +0800

    https://github.com/tangruixing/blog.git

---
+++ Trx.Dao/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trx.Dao
+{
+    public class Article
+    {
+        public Guid ArticleId { get; set; }
+        public string ArticleContent { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime ModifyTime { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
---
+++ Trx.Web/Controllers/BlogController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trx.Dao;
+
+namespace Trx.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            var db = new BlogDBContext();
+            db.Articles.Add(new Article { ArticleContent = "aaa", ArticleId = Guid.NewGuid(), CreateTime = DateTime.Now,ModifyTime = DateTime.Now,IsDelete = false });
+            db.SaveChanges();
+
+            return View(db.Articles.ToList());
+        }
+
+        //
+        // GET: /Blog/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Blog/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Blog/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Blog/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Blog/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Blog/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Blog/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
